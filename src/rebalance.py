f6d4bbcb5642382275df5733f0c7da70593e59fe
yvanst <yvanstu@gmail.com>
2024-01-13 11:56:18 +0800

    add rebalance and stop gain stop loss (#2)
    * update
    
    * update
    
    * update
    
    ---------
    
    Co-authored-by: yifei guo <yifei@yifeis-MacBook-Pro.local>

---
+++ src/rebalance.py
@@ -0,0 +1,39 @@
+class Rebalance:
+    def __init__(
+        self, period, portfolio, factor, blacklist, disable_rebalance=False
+    ) -> None:
+        self.period = period
+        self.portfolio = portfolio
+        self.factor = factor
+        self.blacklist = blacklist
+        self.disable_rebalance = disable_rebalance
+
+    def run(self, cur_date):
+        if self.disable_rebalance:
+            return
+        postition = self.factor.get_position()
+        weight = 0.9 / max(1, len(postition) - len(self.blacklist))
+        postition = [
+            (p, weight) if p not in self.blacklist else (p, 0) for p, w in postition
+        ]
+        position_change = []
+        for security, weight in postition:
+            condition = self.portfolio.security_book[security]["date"] == cur_date
+            original_weight = self.portfolio.security_book[security][condition][
+                "weight"
+            ].iloc[0]
+            position_change.append((security, weight - original_weight))
+
+        # sold first and then buy
+        position_change.sort(key=lambda p: p[1])
+        print(f"rebalance on {cur_date}: {position_change}")
+
+        for security, weight_change in position_change:
+            if weight_change < 0:
+                self.portfolio.reduce_security_weight(
+                    security, abs(weight_change), cur_date
+                )
+            elif weight_change > 0:
+                self.portfolio.add_security_weight(security, weight_change, cur_date)
+            else:
+                pass
--- src/strategy.py
+++ src/strategy.py
@@ -1,3 +1,5 @@
+from abc import ABC, abstractmethod
+import numpy as np
 from enum import Enum
 from dataclasses import dataclass
 import random
@@ -16,26 +18,27 @@ class Order:
     weight: float = 0.0
 
 
-# TODO: strategy should be composable?
-class Strategy:
-    def __init__(self, portfolio):
-        pass
+class Strategy(ABC):
+    def __init__(self, portfolio, blacklist):
+        self.portfolio = portfolio
+        self.blacklist = blacklist
 
+    @abstractmethod
     def get_order(self, target_security, cur_date):
         pass
 
 
 class NoStrategy(Strategy):
-    def __init__(self, portfolio):
-        self.portfolio = portfolio
+    def __init__(self, portfolio, blacklist):
+        super().__init__(portfolio, blacklist)
 
     def get_order(self, target_security, cur_date):
         return Order(OrderType.NOOP)
 
 
 class RandomBuyAndSell(Strategy):
-    def __init__(self, portfolio):
-        self.portfolio = portfolio
+    def __init__(self, portfolio, blacklist):
+        super().__init__(portfolio, blacklist)
         self.state = True
         self.target_security = "RUT"
 
@@ -51,4 +54,33 @@ class RandomBuyAndSell(Strategy):
 
 
 class StopGainAndLoss(Strategy):
-    pass
+    def __init__(self, portfolio, blacklist):
+        super().__init__(portfolio, blacklist)
+        self.set_limit()
+
+    def set_limit(self, gain_limit=1, loss_limit=1):
+        self.gain_limit = gain_limit
+        self.loss_limit = -abs(loss_limit)
+
+    def get_order(self, target_security, cur_date, prev_rebalance_date):
+        security_book = self.portfolio.security_book[target_security]
+        cur_value = security_book[security_book["date"] == cur_date]["value"].iloc[0]
+        if cur_value == 0:
+            return Order(OrderType.NOOP)
+
+        # previous rebalance date?
+        # previous min/max day?
+        start_value = security_book[security_book["date"] == prev_rebalance_date][
+            "value"
+        ].iloc[0]
+        range_return = (cur_value - start_value) / start_value
+
+        if range_return > self.gain_limit or range_return < self.loss_limit:
+            weight = security_book[security_book["date"] == cur_date]["weight"].iloc[0]
+            self.blacklist.append(target_security)
+            if range_return > 0:
+                print(f"{cur_date}: stop gain {target_security}")
+            else:
+                print(f"{cur_date}: stop loss {target_security}")
+            return Order(OrderType.SELL, target_security, weight)
+        return Order(OrderType.NOOP)
